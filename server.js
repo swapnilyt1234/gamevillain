c85d2ed227a225292906e7aa6c68330ecffacc8f
Glitch (gamevillainoff) <none>
2020-07-27 18:58:10 +0000

    👓👨 Updated with Glitch

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const Discord = require("discord.js");
 const tutorialBot = require("./handler/ClientBuilder.js");
 const client = new tutorialBot();
@@ -7,4 +8,21 @@ require("./handler/Event.js")(client);
 client.package = require("./package.json");
 client.on("warn", console.warn);
 client.on("error", console.error);
-client.login(process.env.TOKEN).catch(console.error);
+client.login(process.env.SECRET).catch(console.error);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WE MOVED LOL
