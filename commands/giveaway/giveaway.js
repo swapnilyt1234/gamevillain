3b4ce1b86d7f670255785089083aac12ab88ee25
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-04 11:07:08 +0530

    Update giveaway.js

--- commands/giveaway/giveaway.js
+++ commands/giveaway/giveaway.js
@@ -1,56 +1,60 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js')
 const ms = require("ms");
 
 exports.run = async(client, message, args) => {
-if (!args[0]) return message.channel.send(`You did not specify your time period!`);
-    if (
-      !args[0].endsWith("d") &&
-      !args[0].endsWith("h") &&
-      !args[0].endsWith("m")
-    )
-      return message.channel.send(
-        `You did not use the correct format for the time!`
-      );
-    if (isNaN(args[0][0])) return message.channel.send(`That is not a number!`);
-    let channel = message.mentions.channels.first();
-    if (!channel)
-      return message.channel.send(
-        `I could not find that channel in the guild!`
-      );
-    let prize = args.slice(2).join(" ");
-    if (!prize) return message.channel.send(`No prize specified!`);
-    message.channel.send(`*Giveaway created in ${channel}*`);
-    let Embed = new MessageEmbed()
-      .setTitle(`New giveaway!`)
-      .setDescription(
-        `The user ${message.author} is hosting a giveaway and the prize of the giveaway is  **${prize}**`
-      )
-      .setTimestamp(Date.now() + ms(args[0]))
-      .setColor(`RANDOM`);
-    let m = await channel.send(Embed);
-    m.react("🎉");
-    setTimeout(() => {
-      if (m.reactions.cache.get("🎉").count == 1) {
-        message.channel.send(`Reactions: ${m.reactions.cache.get("🎉").count}`);
-        return message.channel.send(
-          `Not enough people reacted to choose a winner!`
-        );
-      }
 
-      let winner = m.reactions.cache
-        .get("🎉")
-        .users.cache.filter((u) => !u.bot)
-        .random();
-      channel.send(
-        `${winner} won the giveaway with the prize **${prize}**`
-      );
-    }, ms(args[0]));
-  }
-  
+        if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You are not allowed to make giveaways');
+
+        let channel = message.mentions.channels.first();
+
+        if (!channel) return message.channel.send('Please provide a channel');
+
+        let giveawayDuration = args[1];
+
+        if (!giveawayDuration || isNaN(ms(giveawayDuration))) return message.channel.send('Pleae provide a valid time peroid');
+
+        let giveawayWinners = args[2];
+
+        if (isNaN(giveawayWinners) || (parseInt(giveawayWinners) <= 0)) return message.channel.send('Please provide a valid number of winners!');
+
+        let giveawayPrize = args.slice(3).join(" ");
+
+        if (!giveawayPrize) return message.channel.send('Ok then, I\'ll give away nothing');
+
+        client.giveawaysManager.start(channel, {
+            time: ms(giveawayDuration),
+            prize: giveawayPrize,
+            winnerCount: giveawayWinners,
+            hostedBy: client.config.hostedBy ? message.author : null,
+
+            messages: {
+                giveaway: (client.config.everyoneMention ? "@everyone\n\n" : "") + "GIVEAWAY",
+                giveawayEned: (client.config.everyoneMention ? "@everyone\n\n" : "") + "GIVEAWAY ENDED",
+                timeRemaining: "Time remaining: **{duration}**",
+                inviteToParticipate: "React with 🎉 to enter",
+                winMessage: "Congrats {winners}, you won **{prize}**",
+                embedFooter: "Giveaway time!",
+                noWinner: "Couldn't determine a winner",
+                hostedBy: "Hosted by {user}",
+                winners: "winner(s)",
+                endedAt: "Ends at",
+                units: {
+                    seconds: "seconds",
+                    minutes: "minutes",
+                    hours: "hours",
+                    days: "days",
+                    pluralS: false
+                }
+            }
+        })
+
+        message.channel.send(`Giveaway starting in ${channel}`);
+    }
+
   exports.help = {
   name: "giveaway",
   description: "Makes a giveaway",
-  usage: "?giveaway #time #channel #prize"
+  usage: "?giveaway #channel #time #winner #prize"
   }
 
 exports.conf = {
