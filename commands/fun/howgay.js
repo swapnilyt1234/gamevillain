dffaebb77369136c2c57ce468bd111304c24af82
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-04 14:14:31 +0530

    Create howgay.js

---
+++ commands/fun/howgay.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+    let user = message.mentions.users.first() || message.author;
+    let random = Math.floor(Math.random() * 100) + 1;
+    message.channel.send(user + " is " + random + "% gay.")
+}
+
+exports.help = {
+  name: "howgay",
+  description: "Tells how gay you are",
+  usage: "?howgay"
+  }
+
+exports.conf = {
+  aliases: [""],
+  cooldown: 1
+  }
