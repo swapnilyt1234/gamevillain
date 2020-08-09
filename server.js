5338f25cdb5d64030241e763d8e2be74dba49df8
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-09 20:59:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ client.package = require("./package.json");
 client.on("warn", console.warn);
 client.on("error", console.error);
 client.login(process.env.TOKEN).catch(console.error);
+
+const { GiveawaysManager } = require('discord-giveaways');
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./database.json",
+    updateCountdownEvery: 3000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
+        embedColor: "#FF0000",
+        reaction: ":tada:"
+    }
