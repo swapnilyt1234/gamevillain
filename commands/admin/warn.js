9e2d96b5b7af0a3b8dfb5d9344432ba925d875ce
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-09 17:48:42 +0530

    Create warn.js

---
+++ commands/admin/warn.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+exports.run = async(client, message, args) => {
+if(!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send("You should have admin perms to use this command!")
+    }
+    
+    const user = message.mentions.members.first()
+    
+     if(!user) {
+      return message.channel.send("Please Mention the person to who you want to warn - warn @mention <reaosn>")
+    }
+    
+    if(message.mentions.users.first().bot) {
+      return message.channel.send("You can not warn bots")
+    }
+    
+    if(message.author.id === user.id) {
+      return message.channel.send("You can not warn yourself")
+    }
+    
+    if(message.author.id === message.guild.owner.id) {
+      return message.channel.send("You jerk, how you can warn server owner -_-")
+    }
+    
+    const reason = args.slice(1).join(" ")
+
+  if(!reason) {
+      return message.channel.send("Please provide reason to warn - warn @mention <reason>")
+    }
+}
+
+exports.help = {
+  name: "warn",
+  description: "Warns a user",
+  usage: "?warn <@user> #reason",
+  example: "?warn @swapnil#0001 #reason"
+};
+
+exports.conf = {
+  aliases: ["warn"],
+  cooldown: 5
+};
