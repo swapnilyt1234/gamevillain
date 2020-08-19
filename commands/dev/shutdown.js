96b25d5d05cf9bbe17f29dd4d4c4f9e09223d772
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-19 17:50:54 +0530

    Create shutdown.js

---
+++ commands/dev/shutdown.js
@@ -0,0 +1,33 @@
+let m = "";
+    await message.channel
+      .send({embed:{title:"Are you sure you want to shutdown this bot?", color:'#FF0000'}})
+      .then(msd => (m = msd));
+    await m.react("719132543026331680");
+    await m.react("719136364616089634");
+    m.awaitReactions(
+      (reaction, user) =>
+        user.id == message.author.id &&
+        (reaction._emoji.id== "719132543026331680" || reaction._emoji.id== "719136364616089634" ),
+      { max: 1, time: 20000 }
+    )
+      .then(async collected => {
+        if (collected.first().emoji.id == "719132543026331680") {
+          await message.channel.send({embed:{title:"Shutting down the bot... Goodbye!", color:'#FF0000'}})
+          await console.log("Bot has been shut down")
+          bot.destroy();
+        } else message.channel.send({embed:{title:"Aight, shutdown has been canceled.", color:'#00FF00'}});
+      })
+      .catch(() => {
+        message.channel.send("Alright,shutdown has been canceled")
+        })
+
+exports.help = {
+  name: "kill",
+  description: "shutdown",
+  usage: "?kill",
+};
+
+exports.conf = {
+  aliases: ["shutdown"],
+  cooldown: 5
+};
