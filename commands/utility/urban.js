97e277b5ca7312d6871ef8f5c9ee351ba81dbb2a
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-20 12:37:19 +0530

    Create urban.js

---
+++ commands/utility/urban.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+const urban = require("relevant-urban")
+
+exports.run = async(client, message, args) => {
+  if (!args[0]) return message.channel.send("Please specify a word to search")
+
+  let result = await urban(args[0]).catch(e => {
+    return message.channel.send("Unknown word phrase of **${args[0]}**, please try again later")
+
+    const embed = new Discord.MessageEmbed
+      .setColor(0x7289DA)
+      .setTitle(result.word)
+      .setURL(result.urbanURL)
+      .setDescription(`**Defination:** \n**${result.defination}** \n\n**Example:** \n**${result.example}**`)
+      .addField("Author", res.author, true)
+      .addField("Rating", `👍 ${res.thumbsUp.toLocaleString()} | 👎 ${res.thumbsDown.LocaleString()}`)});
+
+  if (result.tags.length > 0 && result.tags.join(" ").length < 1024) {
+    embed.addField("Tags", result.tags.join(", "), true);
+  }
+
+  return message.channel.send(embed)
+}
+
+exports.help = {
+  name: "urban",
+  description: "Give information about a particular word",
+  usage: "?urban india"
+  }
+
+exports.conf = {
+  aliases: [],
+  cooldown: 1
+  }
