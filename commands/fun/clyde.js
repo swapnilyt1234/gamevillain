f4e0de52fd19f3c49d19ce76506ff67c9072fcec
swapnilyt1234 <68676108+swapnilyt1234@users.noreply.github.com>
2020-08-23 14:29:45 +0530

    Update clyde.js

--- commands/fun/clyde.js
+++ commands/fun/clyde.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js')
 exports.run = async (client, message, args) => {
   if (!args[0]) return message.channel.send('What do you want clyde to say?');
   let clydeMessage = args.slice(0).join(" ");
-  let encodedLink = encodeURI("https://ctk-api.herokuapp.com/clyde/$`{clydeMessage}`");
+  let encodedLink = encodeURI(`https://ctk-api.herokuapp.com/clyde/${clydeMessage}`);
     const clydeEmbed = new MessageEmbed()
     .setTitle('Clyde!')
     .setImage(encodedLink);
