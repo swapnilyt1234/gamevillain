98711c4be80181a6a38d7bdfd29ef5d112f270b9
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-11 23:43:14 +0530

    Update joke.js

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,13 +1,20 @@
 const discord = require("discord.js")
 const client = new discord.Client()
 const idJokes = require('give-me-a-joke');
+link = "https://v2.jokeapi.dev/joke/Any"
 
 exports.run = async (client, message, args) => {
-  let category = "blonde";
-idJokes.todayJoke (category, function(joke) {
-    message.channel.send(joke)
-  });
+  axios.get(link).then(res => {
+  message.channel.send({embeds:[{title:res.body.setup, description: res.body.delivery}]})
+}) 
+
 }
+
+const embed = new Discord.MessageEmbed()
+      .setTitle(joke.setup)
+      .setDescription(joke.delivery)
+
+message.channel.send(embed);
 
 exports.help = {
   name: "joke",
