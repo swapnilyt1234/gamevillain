59cf398cd02902ad55119b9a2e2e4c4b96d24cfa
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-12 11:28:33 +0530

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
   message.channel.send({
     embed: {
       title: "Invite me!",
-      description: `[Click here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`,
+      description: `[Click here](https://discord.com/api/oauth2/authorize?client_id=951485159658631228&permissions=8&scope=bot)`,
       color: "00ff00"
     }
   });
