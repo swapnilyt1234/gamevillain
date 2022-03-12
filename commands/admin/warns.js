f684ea4c32ac639cfde2094cbfc6caf71f942ace
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-12 18:42:57 +0530

    Update warns.js

--- commands/admin/warns.js
+++ commands/admin/warns.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if(!user) return message.channel.send('User not found.')
         const reason = args.slice(1).join(" ")
-        mong.findOne({ guildid: message.guild.id, user: user.user.id}, async(err, data) => {
+        db.findOne({ guildid: message.guild.id, user: user.user.id}, async(err, data) => {
             if(err) throw err;
             if(data) {
                 message.channel.send(new MessageEmbed()
@@ -17,11 +17,14 @@ exports.run = async (client, message, args) => {
                             `\`${i + 1}\` | Moderator : ${message.guild.members.cache.get(w.moderator).user.tag}\nReason : ${w.reason}`
                         )
                     )
-                    .setColor("RANDOM")
+                    .setColor("BLUE")
                 )
             } else {
                 message.channel.send('User has no data')
             }
+
+        })
+    }
 
         })
 }
