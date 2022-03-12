c0fdf2019fb06030bd885e3a79f98098146fbb7b
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-12 19:03:11 +0530

    Update reset-warn.js

--- commands/admin/reset-warn.js
+++ commands/admin/reset-warn.js
@@ -1,35 +1,20 @@
 const Discord = require("discord.js");
-const db = require("quick.db");
+const db = require("../../warnsystem.js");
 
 exports.run = async (client, message, args) => {
   
-         if(!message.member.hasPermission("ADMINISTRATOR")) {
-      return message.channel.send("Yopu should have admin perms to use this command")
-    }
-    
-      const user = message.mentions.members.first()
-      
-  if(!user) {
-    return message.channel.send("Please mention the person whose warning you want to reset")
-    }
-    
-    if(message.mentions.users.first().bot) {
-      return message.channel.send("Bot are not allowed to have warnings")
-    }
-  
-   if(message.author.id === user.id) {
-      return message.channel.send("You are not allowed to reset your warnings")
-    }
-  
-    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
-  
- if(warnings === null) {
-      return message.channel.send(`${message.mentions.users.first().username} do not have any warnings`)
-    }
-  
-   db.delete(`warnings_${message.guild.id}_${user.id}`)
-    user.send(`Your all warnings are reseted by ${message.author.username} from ${message.guild.name}`)
-    await message.channel.send(`Reseted all warnings of ${message.mentions.users.first().username}`) //DO NOT FORGET TO USE ASYNC FUNCTION
+         if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permission to use this command.')
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if(!user) return message.channel.send('User not found.')
+        db.findOne({ guildid : message.guild.id, user: user.user.id}, async(err,data) => {
+            if(err) throw err;
+            if(data) {
+                await db.findOneAndDelete({ user : user.user.id, guildid: message.guild.id})
+                message.channel.send(`Cleared ${user.user.tag}'s warns`)
+            } else {
+                message.channel.send('This user does not have any warns in this server!')
+            }
+        })
 }
 
 exports.help = {
