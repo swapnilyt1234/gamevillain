259fb676adc3c2d4b0d784bf43921986857f2eff
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-12 18:53:42 +0530

    Update warn.js

--- commands/admin/warn.js
+++ commands/admin/warn.js
@@ -1,69 +1,39 @@
 const Discord = require("discord.js");
-const mong = require('../../warnsystem.js')
+const db = require('../../warnsystem.js')
 exports.run = async(client, message, args) => {
-            if(!message.member.hasPermission("ADMINISTRATOR")) {
-      return message.channel.send("You should have admin perms to use this command!")
-    }
-    
-    const user = message.mentions.members.first()
-    
-     if(!user) {
-      return message.channel.send("Please Mention the person to who you want to warn - warn @mention <reaosn>")
-    }
-    
-    if(message.mentions.users.first().bot) {
-      return message.channel.send("You can not warn bots")
-    }
-      
-      if(message.author.id === user.id) {
-      return message.channel.send("You can not warn yourself")
-    }
-    
-if(message.guild.owner.id == user.user.id) {
-      return message.channel.send("You jerk, how you can warn server owner -_-")
-    }
-    
-    const reason = args.slice(1).join(" ")
-
-  if(!reason) {
-      return message.channel.send("Please provide reason to warn - warn @mention <reason>")
-    }
-      
-       let warnings = await mong.findOne({
-         mid: `${user.id}_${message.guild.id}`
-       }, (err, mem) => {
-        if(err)console.log(err)
-        if(!mem){
-           let news = new mong({
-             mid: `${user.id}_${message.guild.id}`,
-             warnings: null
-           })
-          news.save()
-        }
-       })
-       
-       if(warnings === 3) {
-      return message.channel.send(`${message.mentions.users.first().username} already reached his/her limit with 3 warnings`)
-    }
-      
-       if(warnings === null) {
-      let news = new mong({
-             mid: `${user.id}_${message.guild.id}`,
-             warnings: 1
-           })
-          news.save()
-      user.send(`You have been warned in **${message.guild.name}** for ${reason}`)
-      await message.channel.send(`You warned **${message.mentions.users.first().username}** for ${reason}`)
-    }
-      else if(warnings !== null) {
-        let news = new mong({
-             mid: `${user.id}_${message.guild.id}`,
-             warnings: 1
-           })
-          news.save()
-       user.send(`You have been warned in **${message.guild.name}** for ${reason}`)
-      await message.channel.send(`You warned **${message.mentions.users.first().username}** for ${reason}`)
-    }
+            if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command.')
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if(!user) return message.channel.send('User not found.')
+        const reason = args.slice(1).join(" ")
+        db.findOne({ guildid: message.guild.id, user: user.user.id}, async(err, data) => {
+            if(err) throw err;
+            if(!data) {
+                data = new db({
+                    guildid: message.guild.id,
+                    user : user.user.id,
+                    content : [
+                        {
+                            moderator : message.author.id,
+                            reason : reason
+                        }
+                    ]
+                })
+            } else {
+                const obj = {
+                    moderator: message.author.id,
+                    reason : reason
+                }
+                data.content.push(obj)
+            }
+            data.save()
+        });
+        user.send(new MessageEmbed()
+            .setDescription(`You have been warned for ${reason}`)
+            .setColor("RED")
+        )
+        message.channel.send(new MessageEmbed()
+            .setDescription(`Warned ${user} for ${reason}`).setColor('BLUE')
+        )
 }
     
 exports.help = {
