c3446ca43b0c72a5bf9e3827e41649f8cda66138
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-12 21:32:47 +0530

    Update remove-warn.js

--- commands/admin/remove-warn.js
+++ commands/admin/remove-warn.js
@@ -4,6 +4,12 @@ const db = require('../../warnsystem.js')
 exports.run = async(client, message, args) => {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permission to use this command.')
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+if(message.mentions.users.first().bot) {
+      return message.channel.send("Bots don't have any warnings!")
+    }
+if(message.author.id === user.id) {
+      return message.channel.send("You can not remove your warnings!")
+    }
         if(!user) return message.channel.send('User not found.')
         db.findOne({ guildid : message.guild.id, user: user.user.id}, async(err,data) => {
             if(err) throw err;
