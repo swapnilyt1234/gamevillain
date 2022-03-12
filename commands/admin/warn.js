001343ff445e298e771f4fe1bf71269c285789eb
CodeWithSwapnil <68676108+swapnilyt1234@users.noreply.github.com>
2022-03-12 21:29:17 +0530

    Update warn.js

--- commands/admin/warn.js
+++ commands/admin/warn.js
@@ -3,6 +3,21 @@ const db = require('../../warnsystem.js')
 exports.run = async(client, message, args) => {
             if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command.')
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+if(!user) {
+      return message.channel.send("Please Mention the person to who you want to warn - warn @mention <reaosn>")
+    }
+
+    if(message.mentions.users.first().bot) {
+      return message.channel.send("You can not warn bots")
+    }
+
+      if(message.author.id === user.id) {
+      return message.channel.send("You can not warn yourself")
+    }
+
+if(message.guild.owner.id == user.user.id) {
+      return message.channel.send("You jerk, how you can warn server owner -_-")
+    }
         if(!user) return message.channel.send('User not found.')
         const reason = args.slice(1).join(" ")
         db.findOne({ guildid: message.guild.id, user: user.user.id}, async(err, data) => {
